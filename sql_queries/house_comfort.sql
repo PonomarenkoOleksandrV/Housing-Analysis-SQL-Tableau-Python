c5718f51dbb10adadbb5509749496912d723bbb3
Oleksandr Ponomarenko <antares111182@gmail.com>
2026-01-18 13:29:13 +0200

    Add files via upload

---
+++ sql_queries/house_comfort.sql
@@ -0,0 +1,10 @@
+SELECT 
+    Id,
+    Fireplaces,
+    GarageCars,
+    WoodDeckSF,
+    PoolArea,
+    SalePrice
+FROM `house-market-analysis-482812.real_estate_analysis.house_prices_final`
+WHERE FullBath > 1;
+
